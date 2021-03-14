93735692a780df7ff1c058ded14febb9bac9dc0f
Инженерка <76725179+EngineeringRoom@users.noreply.github.com>
2021-03-14 15:01:24 +0700

    Add files via upload

---
+++ Sibeng24Regulator.h
@@ -0,0 +1,89 @@
+#ifndef Sibeng24Regulator_h
+#define Sibeng24Regulator_h
+
+// Перечесление Типов регулятора
+enum enumRegulatorType {
+  HEATER,
+  COOLER
+};
+
+// Перечесление Режимов регулятора
+enum enumRegulatorMode {
+  AUTOMODE,
+  MANUALMODE,
+  ERRORMODE
+};
+
+class Sibeng24Regulator {
+
+  
+  protected:
+    uint8_t _id;          // ID класса (порядковый номер регулятора)
+    static uint8_t count; // Счетчик для ID класса (порядковый номер регулятора)
+    
+    uint8_t _RegulatorType; // Тип регулятора (HEATER, COOLER)
+    uint8_t _Mode;         // Режим рулятора (Автоматический/Ручной/Ошибка)(AUTOMODE, MANUALMODE, ERRORMODE)По умолчанию Ручной
+    float _TempIn;      // Температура от датчика температуры
+    float _TempSet;     // Уставка температуры
+    float _Delta;       // Дельта (Гистерезис)
+    bool _State;        // Состояние выхода (Включен/Выключен)(HIGH/LOW)
+    bool _ErrState;     // Состояние выхода (Включен/Выключен)(HIGH/LOW) При ошибке датчика
+    bool _RLL;          // Уровень Логики Реле. Каким сигналом управляется (HIGH/LOW)
+
+  public:
+
+    // Конструктор класса
+    Sibeng24Regulator() {
+      _RegulatorType = HEATER;
+      _RLL = HIGH;
+      _Mode = MANUALMODE;
+
+      _TempIn = 0;
+      _TempSet = 0;
+      _Delta = 1;
+      _State = LOW;
+      _ErrState = LOW;
+
+      count++;
+      _id = count;
+    }
+
+    bool OutRelay();                  // Производит вычисления и выдает результат на Реле
+    bool OutRelay(float TempIn);      // Производит вычисления и выдает результат на Реле
+    bool OutState();                  // Производит вычисления и выдает результат Логики
+    bool OutState(float TempIn);      // Производит вычисления и выдает результат Логики
+
+    void setFactorySettings(); // Сброс настроек в заводские
+    void setAuto();     // Выбираем (Автоматический Режим)
+
+    void setManual();   // Выбираем (Ручной Режим)(Автоматически Состояние на выходи станет Равным Безопасному состояние реле )
+    void setManualON();   // Выбираем (Ручной Режим)(Состояние на выходи станет HIGH)
+    void setManualOFF();   // Выбираем (Ручной Режим)(Автоматически Состояние на выходи станет LOW)
+
+    void setError();    // Выбираем (Ручной Ошибка) По датчику
+    void setErrorState(bool ErrState); // Безопасное состояние реле
+
+    void setType(uint8_t Type);  // Выбираем тип регулятора(HEATER, COOLER)
+    void setRLL(bool RLL);       // Выбираем тип выходного сигнала Уровень Логики Реле. Каким сигналом управляется (HIGH/LOW)
+
+    void setDelta(float Delta);      // Устанавливаем Дельта (Гистерезис)
+    void setTempSet(float TempSet);  // Устанавливаем Уставкe температуры
+    void setTempIn(float TempIn);    // Устанавливаем Температура от датчика температуры
+
+    bool   Alarm();    // Возвращает Сигнал Аварии (от датчика)
+    uint8_t getModeState();  // Возвращает режим регулятора состояние (Автоматический/Ручной/Ошибка)(AUTOMODE, MANUALMODE, ERRORMODE)
+
+    uint8_t getType();  // Возвращает тип регулятора(HEATER, COOLER)
+    bool   getRLL();   // Возвращает тип выходного сигнала Уровень Логики Реле. Каким сигналом управляется (HIGH/LOW)
+
+    float getDelta();    // Возвращает Дельта (Гистерезис)
+    float getTempSet();  // Возвращает Уставкe температуры
+    float getTempIn();   // Возвращает Температура от датчика температуры
+
+    uint8_t getId();     // ID класса (порядковый номер регулятора)
+    static uint8_t getCount();  // Счетчик для ID класса (порядковый номер регулятора)
+    
+    void printSet();// Выводит в Serial все настройки регулятора
+}; // Sibeng24Regulator
+
+#endif Sibeng24Regulator_h
---
+++ Sibeng24RegulatorEEPROM_ESP.h
@@ -0,0 +1,82 @@
+#ifndef Sibeng24RegulatorEEPROM_ESP_h
+#define Sibeng24RegulatorEEPROM_ESP_h
+
+#include <Sibeng24RegulatorEEPROM.h>
+
+////////////////////////////////////////////////
+// Работа с Sibeng24RegulatorEEPROM_ESP
+
+class Sibeng24RegulatorEEPROM_ESP : public Sibeng24RegulatorEEPROM {
+
+ 
+  protected:
+
+    // Сохраняем данные в EEPROM
+    void SaveSettigsEEPROM(uint16_t addr, uint8_t &value) {
+      EEPROM.begin(EEPROMaddr); 
+        EEPROM_writeAnything(addr, value);
+      EEPROM.end();
+    }
+    // Сохраняем данные в EEPROM
+    void SaveSettigsEEPROM(uint16_t addr, float &value) {
+      EEPROM.begin(EEPROMaddr);
+        EEPROM.put(addr,  value);
+      EEPROM.end();
+    }
+    // Сохраняем данные в EEPROM
+    void SaveSettigsEEPROM(uint16_t addr, bool &value) {
+      EEPROM.begin(EEPROMaddr);
+        EEPROM_writeAnything(addr, value);
+      EEPROM.end();
+    }
+
+     // Проверяем что записанно в нулевой ячеке EEPROM
+     // Проверяем инициализированна ли память
+    bool checkEEPROM(){
+      uint16_t check;
+      EEPROM.begin(sizeof(EEPROMcheck));
+      EEPROM_readAnything(0, check);
+      EEPROM.end();
+      if(EEPROMcheck == check){
+        return true;
+      }
+      else{
+        if(count == _id){
+          EEPROM.begin(sizeof(EEPROMcheck));
+          EEPROM_writeAnything(0, EEPROMcheck);
+          EEPROM.end();
+        }
+        return false;
+       }
+    }
+
+public:
+
+    Sibeng24RegulatorEEPROM_ESP() {
+      initEEPROM();
+    }
+protected:    
+    // Читаем все настройки из EEPROM
+    void ReadAllSettingsEEPROM() {
+      EEPROM.begin(EEPROMaddr);
+        EEPROM_readAnything(_a[a_RegulatorType], _RegulatorType);
+        EEPROM_readAnything(_a[a_Mode], _Mode);
+        EEPROM_readAnything(_a[a_ErrState], _ErrState);
+        EEPROM_readAnything(_a[a_RLL], _RLL);
+  
+        EEPROM.get(_a[a_TempSet],  _TempSet);
+        EEPROM.get(_a[a_Delta],   _Delta);
+      EEPROM.end();
+    }
+public:
+    // Сбросить настройки всех Экземпляров класса после перезагрузки устройства
+    void setAllFactorySettings(){
+      EEPROMcheck++;
+      EEPROM.begin(EEPROMaddr);
+      EEPROM_writeAnything(0, EEPROMcheck);
+      EEPROM.end();
+    }
+
+}; // Sibeng24RegulatorEEPROM_ESP
+
+#endif Sibeng24RegulatorEEPROM_ESP_h
